6bb0749aa69614351b7edbaeded228c691695244
Jon Bullock <jondooger@gmail.com>
2015-02-12 20:10:07 -0700

    Add simple spring boot app.

---
+++ src/main/java/models/Image.java
@@ -0,0 +1,17 @@
+package models;
+
+public class Image {
+    public Image(String url) {
+        this.url = url;
+    }
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
