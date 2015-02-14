fdb6129b2d3ca8617eedfc058e04c05b878c7510
Jon Bullock <jondooger@gmail.com>
2015-02-14 12:40:18 -0700

    Add unstyled valentine view.

--- src/main/java/com/jonbullock/vday/controllers/HomeController.java
+++ src/main/java/com/jonbullock/vday/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package com.jonbullock.vday.controllers;
 
 import com.jonbullock.vday.models.Image;
+import com.jonbullock.vday.services.ImageService;
+import com.jonbullock.vday.services.PunService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,10 +11,23 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+    private final ImageService imageService = new ImageService();
+    private final PunService punService = new PunService();
+
     @RequestMapping("/")
     public ModelAndView index() {
         ModelAndView mav = new ModelAndView("index");
-        mav.addObject("image", new Image(1, "www.google.com", "cc", null));
+        mav.addObject("image", new Image(1, "fake", "www.google.com", "cc", null));
+        return mav;
+    }
+
+    @RequestMapping("/valentine")
+    public ModelAndView getValentine() {
+        ModelAndView mav = new ModelAndView("valentine");
+        Image image = imageService.getRandomImage();
+        mav.addObject("image", image);
+        mav.addObject("imageUrl", "/images/" + image.getId());
+        mav.addObject("pun", punService.getRandomPunFor(image.getTags()));
         return mav;
     }
 }
